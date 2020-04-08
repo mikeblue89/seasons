40611274656f6efa3051bca780ed6608798b7673
mikeblue89 <mich.esp1989@gmail.com>
2020-04-07 23:00:06 -0600

    adding new elements

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,14 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
 
-    window.navigator.geolocation.getCurrentPosition(
-        position => console.log(position),
-        err => console.log(err)
-    );
+class App extends React.Component {
 
-    return <div>Hi there!</div>
+    constructor(props) {
+        super(props);
+        this.state = { latitud: null, errorMessage: '' };
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+
+                //In this line we call setState
+                this.setState({
+                    latitud: position.coords.latitude,
+                    longitud: position.coords.longitude
+                });
+            },
+            err => {
+                this.setState({ errorMessage: err.message });
+            }
+        );
+    }
+
+    //React says we need to define render
+    render() {
+        if (this.state.errorMessage && !this.state.latitud) {
+            return <div>Error: {this.state.errorMessage}</div>;
+        }
+
+        if (!this.state.errorMessage && this.state.latitud) {
+            return <div>Latitude: {this.state.latitud}</div>;
+        }
+
+        return <div>Loading!</div>;
+    }
 }
 
 ReactDOM.render(
